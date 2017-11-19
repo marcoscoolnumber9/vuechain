7237a6d83998f4d9bf514b7fb20c19c5b23e1cd9
Marcos Roig <marcosroig@MacBook-Pro-de-Marcos.local>
2017-11-19 17:33:49 +0100

    uploading all files

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\User;
+
+class UserController extends Controller
+{
+    public function profile()
+    {
+        $user = Auth::user();
+        return $user;
+    }
+}
